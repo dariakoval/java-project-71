463c1e73c49deb23f3913aab899c0df5be33d04c
dariakoval <dasharibkavmore666@mail.ru>
2023-04-26 17:06:57 +0400

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,14 +6,18 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
-import java.math.BigInteger;
-import java.nio.file.Files;
-import java.security.MessageDigest;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer>  {
+    @Option(names = { "-f", "--format" }, description = "output format [default: stylish]")
+    private String format;
+    @Parameters(paramLabel = "filepath1", description = "path to first file", defaultValue = "/etc/hosts")
+    private File file1 = new File("/etc/hosts");
+    @Parameters(paramLabel = "filepath2", description = "path to second file", defaultValue = "/etc/hosts")
+    private File file2 = new File("/etc/hosts");
+
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
