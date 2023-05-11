6afea8dceeac13b484a6067c68936f77647581a8
dariakoval <dasharibkavmore666@mail.ru>
2023-05-11 13:04:26 +0400

    update files

--- app/src/main/java/hexlet/code/formatters/Plain.java
+++ app/src/main/java/hexlet/code/formatters/Plain.java
@@ -4,6 +4,36 @@ import java.util.Map;
 
 public class Plain {
     public static String genPlain(Map<String, Object> map1, Map<String, Object> map2, Map<String, String> mapDiff) {
-        return "";
+        StringBuilder result = new StringBuilder();
+        for (String key: mapDiff.keySet()) {
+            if (mapDiff.get(key).equals("added")) {
+                result.append("\nProperty '").append(key).append("' was added with value: ").
+                        append(toSpecialString(map2.get(key)));
+            } else if (mapDiff.get(key).equals("deleted")) {
+                result.append("\nProperty '").append(key).append("' was removed");
+            } else if (mapDiff.get(key).equals("changed")) {
+                result.append("\nProperty '").append(key).append("' was updated. From ").
+                        append(toSpecialString(map1.get(key))).append(" to ").
+                        append(toSpecialString(map2.get(key)));
+            }
+        }
+        return result.delete(0, 1).toString();
+    }
+    public static String toSpecialString(Object obj) {
+        String result;
+        if (obj instanceof Map<?, ?>) {
+            result = "[complex value]";
+        } else if (obj instanceof String) {
+            result = "'" + obj + "'";
+        } else if (obj instanceof Integer) {
+            result = obj.toString();
+        } else if (obj == null) {
+            result = null;
+        } else if (obj instanceof Boolean) {
+            result = obj.toString();
+        } else {
+            result = "[complex value]";
+        }
+        return result;
     }
 }
--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -69,4 +69,23 @@ class DifferTest {
                 }""";
         assertThat(Differ.generate(filePath1, filePath2, "stylish")).isEqualTo(expected);
     }
+    @Test
+    void testGenerate3() throws Exception {
+        String filePath1 = "src/test/resources/file10.json";
+        String filePath2 = "src/test/resources/file20.json";
+        String expected = "Property 'chars2' was updated. From [complex value] to false"
+                + "Property 'checked' was updated. From false to true"
+                + "Property 'default' was updated. From null to [complex value]"
+                + "Property 'id' was updated. From 45 to null"
+                + "Property 'key1' was removed"
+                + "Property 'key2' was added with value: 'value2'"
+                + "Property 'numbers2' was updated. From [complex value] to [complex value]"
+                + "Property 'numbers3' was removed"
+                + "Property 'numbers4' was added with value: [complex value]"
+                + "Property 'obj1' was added with value: [complex value]"
+                + "Property 'setting1' was updated. From 'Some value' to 'Another value'"
+                + "Property 'setting2' was updated. From 200 to 300"
+                + "Property 'setting3' was updated. From true to 'none'";
+        assertThat(Differ.generate(filePath1, filePath2, "plain")).isEqualTo(expected);
+    }
 }
