fbc5d6acead7c332a5ece72f171a68ce8b8b1763
dariakoval <dasharibkavmore666@mail.ru>
2023-05-11 13:22:37 +0400

    update file

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -73,19 +73,20 @@ class DifferTest {
     void testGenerate3() throws Exception {
         String filePath1 = "src/test/resources/file10.json";
         String filePath2 = "src/test/resources/file20.json";
-        String expected = "Property 'chars2' was updated. From [complex value] to false"
-                + "Property 'checked' was updated. From false to true"
-                + "Property 'default' was updated. From null to [complex value]"
-                + "Property 'id' was updated. From 45 to null"
-                + "Property 'key1' was removed"
-                + "Property 'key2' was added with value: 'value2'"
-                + "Property 'numbers2' was updated. From [complex value] to [complex value]"
-                + "Property 'numbers3' was removed"
-                + "Property 'numbers4' was added with value: [complex value]"
-                + "Property 'obj1' was added with value: [complex value]"
-                + "Property 'setting1' was updated. From 'Some value' to 'Another value'"
-                + "Property 'setting2' was updated. From 200 to 300"
-                + "Property 'setting3' was updated. From true to 'none'";
+        String expected = """
+                Property 'chars2' was updated. From [complex value] to false
+                Property 'checked' was updated. From false to true
+                Property 'default' was updated. From null to [complex value]
+                Property 'id' was updated. From 45 to null
+                Property 'key1' was removed
+                Property 'key2' was added with value: 'value2'
+                Property 'numbers2' was updated. From [complex value] to [complex value]
+                Property 'numbers3' was removed
+                Property 'numbers4' was added with value: [complex value]
+                Property 'obj1' was added with value: [complex value]
+                Property 'setting1' was updated. From 'Some value' to 'Another value'
+                Property 'setting2' was updated. From 200 to 300
+                Property 'setting3' was updated. From true to 'none'""";
         assertThat(Differ.generate(filePath1, filePath2, "plain")).isEqualTo(expected);
     }
 }
